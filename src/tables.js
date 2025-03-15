613d3fbf2e196a67d5f0fd34d74add9c41cf17bf
boothe0 <elizabethbooth05@gmail.com>
2025-03-15 10:36:47 -0700

    Fixed switched pros and cons for tailwind table

--- src/tables.js
+++ src/tables.js
@@ -28,8 +28,8 @@ const pythonTableData = [
 const tailwindCSSData = [
     {
         library: "A library with premade css classes that are used directly in html reducing the need for long, external css files.",
-        cons: "Difficult to work with small mobile devices less than 640px and at time unintuitive classes for responsive design.",
         pros: "Easy use of flex and grid as well as convenient padding, margin and border classes.",
+        cons: "Difficult to work with small mobile devices less than 640px and at time unintuitive classes for responsive design.",
     },
 ];
 
