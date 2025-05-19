73806b91ce789910c99f64c273f5b9f270863cdb
boothe0 <elizabethbooth05@gmail.com>
2025-05-19 16:17:11 -0700

    Finished moving js to react

--- src/color_switcher.js
+++
@@ -1,9 +0,0 @@
-const themeCheckbox = document.querySelector(
-  '#theme-slider input[type="checkbox"]'
-);
-
-let body = document.getElementById("theme_changer");
-
-themeCheckbox.addEventListener("change", () => {
-  body.classList.toggle("bg-[oklch(95.4%_0.038_75.164)]");
-});
